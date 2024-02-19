4485f615bf81a338c7a59fa16127fa1f50add418
EmmGlezz <contact@emilianogonzalez.com>
2024-02-20 10:55:41 +1100

    Added react-icons and react-router-dom dependencies

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   </React.StrictMode>,
+  document.getElementById('root')
 )
